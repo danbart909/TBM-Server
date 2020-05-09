abdebcc7d89595b7ec5c30db56d18bb14de8ae7c
Dan Bartlett <danbart909@gmail.com>
2020-05-08 23:09:48 -0400

    change initial database url

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   API_TOKEN: process.env.API_TOKEN || 0000,
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres:401923@localhost/database',
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres:401923@localhost/database-test'
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres:401923@localhost/tbm',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres:401923@localhost/tbm-test'
 }
