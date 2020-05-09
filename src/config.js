2026a3b53e6078a49a086a4d6ae29b2a5af58ba6
Dan Bartlett <danbart909@gmail.com>
2020-05-09 12:48:09 -0400

    db update

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   API_TOKEN: process.env.API_TOKEN || 0000,
-  DATABASE_URL: process.env.DATABASE_URL || 'postgres://nqbolyypkxkepc:79b7464e689390907efce8739cd2b50c41e987319b52e9ac3c4d3f662d551831@ec2-52-202-146-43.compute-1.amazonaws.com:5432/d2tvhidlfaub47',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres:401923@localhost/tbm',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres:401923@localhost/tbm-test'
 }
